280ed07ef27d9195b73468e8b2d37508685fb96a
BoredDany <141600976+BoredDany@users.noreply.github.com>
2023-10-25 06:20:19 -0700

    Update TxtFile.java

--- BridgePattern/src/Implementations/TxtFile.java
+++ BridgePattern/src/Implementations/TxtFile.java
@@ -6,5 +6,9 @@ public class TxtFile implements SaveInfoInt {
     @Override
     public void saveInfo(String data) {
         System.out.println("Saving info in txt file");
+        FileWriter fileWriter = new FileWriter("file.txt");
+        fileWriter.write(data);
+        fileWriter.flush();
+        fileWriter.close();
     }
 }
