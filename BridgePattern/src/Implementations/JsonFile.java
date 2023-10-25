ef6e373d17c5ee695c3b8aecd805fdf3920b8534
BoredDany <141600976+BoredDany@users.noreply.github.com>
2023-10-25 06:18:03 -0700

    Update JsonFile.java

--- BridgePattern/src/Implementations/JsonFile.java
+++ BridgePattern/src/Implementations/JsonFile.java
@@ -6,5 +6,9 @@ public class JsonFile implements SaveInfoInt {
     @Override
     public void saveInfo(String data) {
         System.out.println("Saving info in json file");
+        FileWriter fileWriter = new FileWriter("file.json");
+        fileWriter.write(data);
+        fileWriter.flush();
+        fileWriter.close();
     }
 }
